d38fcf2b98a2c5609dd0d8c75da76c5ad2676ebe
Brandon Lin <brandonlin9716@gmail.com>
2024-07-16 13:03:01 -0700

    Add addition, subtraction, multiplication, and division functions

---
+++ javascript.js
@@ -0,0 +1,15 @@
+function add(a, b) {
+    return Number(a) + Number(b);
+}
+
+function subtract(a, b) {
+    return Number(a) - Number(b);
+}
+
+function multiply(a, b) {
+    return Number(a) * Number(b);
+}
+
+function divide(a, b) {
+    return Number(a) / Number(b);
+}
